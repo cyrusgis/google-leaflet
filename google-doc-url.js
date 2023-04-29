7770f923074f9681613b46f0ab20b915d05f3489
cyrusgis <98184452+cyrusgis@users.noreply.github.com>
2023-04-28 17:55:51 -0700

    Add files via upload

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Q8nq-R8Ve76a232c7fCzfULuP-94xvHzlAtV-XrlmaA/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyD9EyGsk_lahxv7dEQfT_lO9IwGNyWXHhg';
